519ef1ecb5bd58f1c6a0fa1fbfe4a1b50773327d
Yarik Shelest <yarikcanhelpme@gmail.com>
2019-08-16 12:57:29 +0300

    add controller

--- backend-book-shop/books-store/src/controllers/books.controller.ts
+++ backend-book-shop/books-store/src/controllers/books.controller.ts
@@ -1,5 +1,16 @@
-import { Controller, Post, Get} from '@nestjs/common';
+import { Controller, Body, Post} from '@nestjs/common';
+import { Books } from 'src/model/books.model';
 import { BooksService } from 'src/services/books.service';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+    constructor(private booksService: BooksService) { }
+
+    @Post('create')
+    async addBook(@Body() Book: Books) {
+        const book = await this.booksService.create(Book);
+        return book;
+    }
+}
+
+
